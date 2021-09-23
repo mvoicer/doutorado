2c7f48fc50b599a212c3d97abdb4b9ec2d3d59f6
Marcos Alves <m.voicer@gmail>
2021-09-23 01:07:15 -0300

    Added a function to calculate the cosine similarity

--- distance.py
+++ distance.py
@@ -7,21 +7,28 @@ class Distance:
     def __init__(self, df):
         self.df = df
 
-    def rec_euclidean(self):
+    def euclidean(self):
         """
-        Calculate the euclidean distance among all the solutions. CODE NEED TO BE OPTIMIZED.
+        Calculate the euclidean distance among all the solutions.
         Input: Pareto front solutions
         Output: Squared matrix with the distance among the solutions.
         """
+        # TODO: optimize code - não é necessário calcular a diagnonal e nem os dois lados da matriz
         df_aux = pd.DataFrame(np.zeros((self.df.shape[0], self.df.shape[0])))
         for r in range(self.df.shape[0]):
             for c in range(self.df.shape[0]):
                 df_aux.loc[r, c] = distance.euclidean(self.df.iloc[r], self.df.iloc[c])
         return df_aux
 
-    # def cosine(self):
-    #     """
-    #     Compute the cosine similarity
-    #     :return: cosine similarity
-    #     """
-    #     return 1 - spatial.distance.cosine(self.rank1, self.rank2)
+    def cosine(self):
+        """
+        Compute the cosine similarity
+        :return: cosine similarity
+        """
+        df_aux = pd.DataFrame(np.zeros((self.df.shape[0], self.df.shape[0])))
+        for r in range(self.df.shape[0]):
+            for c in range(self.df.shape[0]):
+                df_aux.loc[r, c] = 1 - distance.cosine(self.df.iloc[r], self.df.iloc[c])
+        return df_aux
+
+
