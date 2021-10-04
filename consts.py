d632701a5f8e231e7f6b5c92bd919df4531dadab
Marcos Alves <m.voicer@gmail>
2021-10-04 17:01:04 -0300

    Add if_true and if_not_true to make my life easier.

--- consts.py
+++ consts.py
@@ -1,10 +1,19 @@
-n_executions = 2                # número de vezes que vai rodar o algoritmo para tirar a média
-TOTAL = 31                      # total de amostras a serem avaliadas a cada N_REC
-N_REC: int = 5                  # n_samples to be evaluated
-CV: int = 5                     # number of cross-validation
-test_size: float = 0.2          # 80% train and 20% test
-TOP_N = N_REC                   # top n solutions
-tau_all: float = 1.
+if True:
+    n_executions = 4                # número de vezes que vai rodar o algoritmo para tirar a média
+    total_samples_per_rec = 51      # total de amostras a serem avaliadas a cada n_rec
+    n_rec: int = 5                  # numero de amostras que são apresentadas ao decisor por vez
+    CV: int = 5                     # number of cross-validation
+    test_size: float = 0.2          # 80% train and 20% test
+    top_n = n_rec                   # top n solutions
+    max_sample = 100
+if not True:
+    n_executions = 3                # número de vezes que vai rodar o algoritmo para tirar a média
+    total_samples_per_rec = 11      # total de amostras a serem avaliadas a cada n_rec
+    n_rec: int = 2                  # numero de amostras que são apresentadas ao decisor por vez
+    CV: int = 2                     # number of cross-validation
+    test_size: float = 0.2          # 80% train and 20% test
+    top_n = n_rec                   # top n solutions
+    max_sample = 15
 
 results = {'aleatory': {'tau': [],
                         'cosine': [],
