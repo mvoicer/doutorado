799be738c81a32511554b825f27bb31b0954fc3f
Marcos Alves <m.voicer@gmail>
2021-10-29 01:31:44 -0300

    Correções para ajustar as novas classes/métodos

--- main.py
+++ main.py
@@ -5,69 +5,81 @@ from tqdm import tqdm
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-from preferences import notas_pref
-from ahp import ahp
-from data_preparation import create_subsample
+from data_preparation import create_subsample, merge_matrices, _create_subsample, _merge_matrices
 from fine_tunning import fine_tunning
-from data_preparation import merge_matrices
 from correlation import *
 from consts import *
 from distance import Distance
 from metrics import mse, rmse, r2, mape, accuracy
+from mcdm import Gera_Pc_Mcdm, Mcdm_ranking
 
 # NSGAIII_GPD03_M2_DEC.CSV -- 2 objetivos, convexa e multimodal
 # NSGAIII_GPD04_M3_DEC.CSV -- 3 objetivos
 df_var = pd.read_csv("data/NSGAIII_GPD04_M2_DEC.CSV", header=None)  # decision variables
-df_var = df_var.iloc[0:105, :].round(5)
+df_var = df_var.iloc[0:max_sample, :].round(5)
 df_obj = pd.read_csv('data/NSGAIII_GPD04_M2_OBJ.CSV', header=None)  # values in Pareto front
-df_obj = df_obj.iloc[0:105, :].round(5)
+df_obj = df_obj.iloc[0:max_sample, :].round(5)
 
 npop, nvar = df_var.shape
 nobj = df_obj.shape[1]
 
 # Calculate the distance among solutions
-df_dist = Distance(df_obj).euclidean()
+if not True:
+    df_dist = Distance(df_obj).euclidean()
+    wg = 'euclidean'
+if True:
+    df_dist = Distance(df_obj).cosine()
+    wg = 'cosine'
 
 # Generate the preferences
-df_obj = df_obj.to_numpy()
-df_pref = notas_pref(df_obj)
-df_obj = pd.DataFrame(df_obj)
+if True:
+    pc_matrix = Gera_Pc_Mcdm(df=df_obj, weights=[.5, .5], cb=['cost', 'cost']).ahp_pc()
+if not True:
+    pc_matrix = Gera_Pc_Mcdm(df=df_obj, weights=[.5, .5], cb=['cost', 'cost']).promethee_ii_pc()
 
-# Apply the classical AHP to get the ranking
-rank_ahp = ahp(df_pref).index
+# plt.scatter(df_obj.loc[:, 0], df_obj.loc[:, 1], color='grey', marker='o')  # available
+# plt.scatter(df_obj.loc[rank_mcdm[:10], 0], df_obj.loc[rank_mcdm[:10], 1], color='blue', marker='v')  # primeiro elemento
+# # plt.scatter(df_obj.loc[rank_mcdm[-1:], 0], df_obj.loc[rank_mcdm[-1:], 1], color='red', marker='v')  # ultimo elemento
+# plt.show()
 
 # Generate the index to be evaluated
-index = list(df_var.index)
-# Aleatory ranking
-aleatory = index.copy()
-random.shuffle(aleatory)
-rank_aleatory = ahp(df_var.loc[aleatory]).index
+alternatives = list(df_var.index)
 
-for _ in range(n_executions):
-    for r in tqdm(recomm_engines):
+# Initialize an arbitrary ranking to check convergence
+rank_aleatory = alternatives.copy()
+random.shuffle(rank_aleatory)
+
+for r in recomm_engines:
+    for exc in tqdm(range(n_executions)):
+        print("\nEngine/execution: ", r, exc)
+        # Recommended solutions
         Q = []
-        random.shuffle(aleatory)
-        for aux in range(N_REC, TOTAL, N_REC):
+        lista_aleatoria = alternatives.copy()
+        random.shuffle(lista_aleatoria)
+        for aux in range(n_rec, total_samples_per_rec, n_rec):
             if r == 'aleatory' or (r == 'euclidean' and len(Q) == 0):
-                Q = aleatory[0:aux]
-                N_Q = [x for x in index if x not in Q]
+                Q = lista_aleatoria[0:aux]
+                N_Q = [x for x in alternatives if x not in Q]
             elif r == 'euclidean':
-                # recommend most similar (based on euclidean distance)
-                most_similar = df_dist[rank_aleatory[0:aux]].sum(axis=1).sort_values(ascending=True)[0:N_REC].index.to_list()
-                most_similar = most_similar[:N_REC]
+                # calculate most similar
+                most_similar = df_dist[rank_aleatory[0:aux]].sum(axis=1).sort_values(ascending=True)[
+                               0:n_rec * 2].index.to_list()
 
                 # Define Q and N-Q indexes
-                Q = list(Q)
+                temp = len(Q) + n_rec
                 for q in most_similar:
-                    Q.append(q)
-                N_Q = [x for x in index if x not in Q]
+                    if (len(Q) < temp) & (q not in Q):
+                        Q.append(q)
+                    else:
+                        continue
+                N_Q = [x for x in alternatives if x not in Q]
 
             # Train
-            df_Q = create_subsample(df_var=df_var, df_pref=df_pref, nobj=nobj, index=Q)
+            df_Q = create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=Q)
             X_train = df_Q.iloc[:, :-nobj]  # to predict
             y_train = df_Q.iloc[:, -nobj:]  # real targets
             # Test
-            df_N_Q = create_subsample(df_var=df_var, df_pref=df_pref, nobj=nobj, index=N_Q)
+            df_N_Q = create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=N_Q)
             X_test = df_N_Q.iloc[:, :-nobj]  # to predict
             y_test = df_N_Q.iloc[:, -nobj:]  # real targets
 
@@ -76,7 +88,7 @@ for _ in range(n_executions):
                 tuned_model = pickle.load(fp)
 
             # # Fine tunning and save best model
-            # if len(results[r]['tau']) <= int(TOTAL / N_REC):
+            # if len(results[r]['tau']) <= int(total_samples_per_rec / n_rec):
             #     tuned_model = fine_tunning(CV, X_train, y_train)
             #     with open("tuned_model_"+r+".pkl", 'wb') as arq:
             #         pickle.dump(tuned_model, arq)
@@ -99,37 +111,29 @@ for _ in range(n_executions):
             results[r]['mape'].append(mape(y_pred, y_test))
 
             # Merge the predictions of the df train and df test
-            df_merged = merge_matrices(N_Q, df_pref, y_pred, nobj, npop)
+            df_merged = merge_matrices(N_Q, pc_matrix, y_pred)
 
             # Employ AHP in the predicted (mixed with preferences) dataset
-            rank_predicted = ahp(df_merged).index
+            rank_predicted = Mcdm_ranking(df_merged).ahp_ranking()
 
             # Computing tau similarity
             results[r]['tau'].append(norm_kendall(rank_aleatory, rank_predicted))
-
-            # Plot pareto front and recommendations
-            df_obj = pd.DataFrame(df_obj)
-            plt.scatter(df_obj.loc[:, 0], df_obj.loc[:, 1], color='grey', marker='o')  # available
-            plt.scatter(df_obj.loc[Q[0:aux], 0], df_obj.loc[Q[0:aux], 1], color='orange', marker='*')  # recommended
-            plt.scatter(df_obj.loc[rank_predicted[0:aux], 0], df_obj.loc[rank_predicted[0:aux], 1], color='red', marker='^')  # top ranked
-            plt.scatter(df_obj.loc[rank_ahp[0:aux], 0], df_obj.loc[rank_ahp[0:aux], 1], color='blue', marker='v')  # ahp
-            plt.legend(["Available", 'Recommended', "Top ranked", 'AHP'])
-            plt.show()
+            results[r]['rho'].append(spearman_rho(rank_aleatory, rank_predicted))
 
             # Update the ranking
             rank_aleatory = rank_predicted
 
 euc = pd.DataFrame(results['euclidean']['tau'])
-euc['recomm'] = 'euclidean'
-euc['iteracao'] = list(range(0, int(TOTAL / N_REC))) * n_executions
+euc['recommendation'] = wg
+euc['iteracao'] = list(range(0, int(total_samples_per_rec / n_rec))) * n_executions
 
 ale = pd.DataFrame(results['aleatory']['tau'])
-ale['recomm'] = 'aleatory'
-ale['iteracao'] = list(range(0, int(TOTAL / N_REC))) * n_executions
+ale['recommendation'] = 'aleatory'
+ale['iteracao'] = list(range(0, int(total_samples_per_rec / n_rec))) * n_executions
 
 data = pd.concat([euc, ale])
 
-sns.boxplot(data=data, hue='recomm', x='iteracao', y=0)
+sns.boxplot(data=data, hue='recommendation', x='iteracao', y=0)
 plt.xlabel("Iteration")
 plt.ylabel("Similarity")
 plt.ylim(bottom=0, top=1)
