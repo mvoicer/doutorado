21a5c6e6dd90a3e3752b45a12d10fb3f550d7ac8
Marcos Alves <m.voicer@gmail>
2021-10-29 01:26:04 -0300

    Added two new functions (variations) that do not return the main diagonal. If A1 and A2, return only A1+A2 and A2+A1

--- data_preparation.py
+++ data_preparation.py
@@ -5,11 +5,6 @@ import numpy as np
 def create_subsample(df_var, df_pref, nobj, index):
     """
     Create sub-dataframes with the features (alternatives) and target (value in the objective space).
-    :param df_var:
-    :param df_pref:
-    :param nobj:
-    :param index:
-    :return:
     """
 
     # Create a df_aux that receive the features concatenated (objectives) and targets (preference)
@@ -23,6 +18,22 @@ def create_subsample(df_var, df_pref, nobj, index):
             cont += 1
     return sub_df
 
+def _create_subsample(df_var, df_pref, nobj, index):
+    """
+    Create sub-dataframes with the features (alternatives) and target (value in the objective space).
+    """
+
+    # Create a df_aux that receive the features concatenated (objectives) and targets (preference)
+    sub_df = pd.DataFrame(np.zeros((len(index), df_var.shape[1] * 2 + nobj)))
+    cont = 0
+    for i in index:
+        for j in index:
+            if i != j:
+                sub_df.loc[cont] = pd.concat([df_var.loc[i], df_var.loc[j], df_pref.loc[i, j]], axis=0, ignore_index=True)
+            else:
+                continue
+            cont += 1
+    return sub_df
 
 def merge_matrices(idx_N_Q, preference_matrix, ml_predicted):
     """
@@ -41,6 +52,30 @@ def merge_matrices(idx_N_Q, preference_matrix, ml_predicted):
     for i in idx_N_Q:
         for k in idx_N_Q:
             comb_idx.append(tuple([i, k]))
+
+    results = pd.DataFrame()
+    x = 0
+    for _ in range(0, df_merged.shape[1], df_merged.shape[0]):
+        m = df_merged.iloc[:, nobj:nobj+df_merged.shape[0]].to_numpy()
+
+        for i, idx in enumerate(comb_idx):
+            m[idx] = ml_predicted.values[i, x]
+        x += 1
+        m = pd.DataFrame(m)
+        results = pd.concat([results, m], ignore_index=False, axis=1)
+    return results
+
+def _merge_matrices(idx_N_Q, preference_matrix, ml_predicted):
+    df_merged = preference_matrix.copy()
+    nobj = ml_predicted.shape[1]
+
+    comb_idx = []
+    for i in idx_N_Q:
+        for k in idx_N_Q:
+            if i != k:
+                comb_idx.append(tuple([i, k]))
+            else:
+                continue
 
     results = pd.DataFrame()
     x = 0
