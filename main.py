f839090d079ad4c2696fe191772702403b7090b1
Marcos Alves <m.voicer@gmail>
2021-12-06 21:55:02 -0300

    Implemented new types of recommendations and new plot results (tau, rho and rmse)

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from metrics import mse, rmse, r2, mape, accuracy
 from mcdm import Gera_Pc_Mcdm, Mcdm_ranking
 from sklearn.preprocessing import StandardScaler, Normalizer
 np.seterr(divide='ignore', invalid='ignore')    # ignora erros de divisão por 0
+from sklearn.linear_model import Lasso, LassoCV, MultiTaskLassoCV
 
 
 # NSGAIII_GPD03_M2_DEC.CSV -- 2 objetivos, convexa e multimodal
@@ -27,8 +28,9 @@ df_obj = df_obj.iloc[0:max_sample, :].round(5)
 npop, nvar = df_var.shape
 nobj = df_obj.shape[1]
 
-tipo_recomendacao = 'Euclidean'  # Cosine, Euclidean
-mcdm_pairwise_method = 'AHP'  # AHP, Promethee
+tipo_recomendacao = 'Euclidean'     # Cosine, Euclidean
+mcdm_pairwise_method = 'AHP'        # AHP, Promethee
+r_pers = 'half_similar_half_dissimilar'             # most_similar, half_similar_half_dissimilar
 
 # Calculate the distance among solutions
 if tipo_recomendacao == 'Cosine':
@@ -67,24 +69,40 @@ for r in recomm_engines:
         print("\nEngine/execution: ", r, exc)
         # Recommended solutions
         Q = []
+        tam = n_rec
         lista_aleatoria = alternatives.copy()
         random.shuffle(lista_aleatoria)
         for aux in range(n_rec, total_samples_per_rec, n_rec):
             if (r == 'aleatory') | (r == 'personalized' and len(Q) == 0):
-                Q = lista_aleatoria[0:aux]
+                Q = lista_aleatoria[0:tam]
                 N_Q = [x for x in alternatives if x not in Q]
             elif r == 'personalized':
+                # Based on distance
                 # calculate most similar
-                most_similar = df_dist[rank_aleatory[0:aux]].sum(axis=1).sort_values(ascending=True)[
-                               0:n_rec * 2].index.to_list()
+                most_similar = df_dist[rank_aleatory[0:tam]].sum(axis=1).sort_values(ascending=True).index.to_list()
+                most_dissimilar = most_similar.copy()
+                most_dissimilar.reverse()
 
-                # Define Q and N-Q indexes
-                temp = len(Q) + n_rec
-                for q in most_similar:
-                    if (len(Q) < temp) & (q not in Q):
-                        Q.append(q)
-                    else:
-                        continue
+                if r_pers == 'most_similar':
+                    # Define Q and N-Q indexes
+                    for q in most_similar:
+                        if (len(Q) <= tam) & (q not in Q):
+                            Q.append(q)
+                        else:
+                            continue
+                elif r_pers == 'half_similar_half_dissimilar':
+                    # Half most similar
+                    for q in most_similar:
+                        if (len(Q) <= tam - 2) & (q not in Q):
+                            Q.append(q)
+                        else:
+                            continue
+                    # Half most dissimilar
+                    for p in most_dissimilar:
+                        if (len(Q) <= tam) & (p not in Q):
+                            Q.append(p)
+                        else:
+                            continue
                 N_Q = [x for x in alternatives if x not in Q]
 
             # Train
@@ -97,13 +115,14 @@ for r in recomm_engines:
             y_test = df_N_Q.iloc[:, -nobj:]  # real targets
 
             # if mcdm_pairwise_method == 'Promethee':
-            #     std = Normalizer().fit(X_train)
-            #     X_train = std.transform(X_train)
-            #     X_test = std.transform(X_test)
+            # std = StandardScaler().fit(X_train)
+            # X_train = std.transform(X_train)
+            # X_test = std.transform(X_test)
 
             # Load trained model
-            with open("models/tuned_model_random.pkl", "rb") as fp:
-                tuned_model = pickle.load(fp)
+            # with open("models/tuned_model_random.pkl", "rb") as fp:
+            #     tuned_model = pickle.load(fp)
+            tuned_model = MultiTaskLassoCV(n_alphas=100, normalize=True, random_state=1, max_iter=60000000, cv=5, n_jobs=-1)
 
             # # # Fine tunning and save best model
             # if len(results[r]['tau']) <= int(total_samples_per_rec / n_rec):
@@ -116,7 +135,7 @@ for r in recomm_engines:
 
             # Model training
             tuned_model.fit(X_train, y_train)
-
+            print('alpha : ', tuned_model.alpha_)
             # Model evaluation
             y_pred = tuned_model.predict(X_test)
             y_pred = pd.DataFrame(y_pred)
@@ -140,13 +159,13 @@ for r in recomm_engines:
             else:
                 raise ValueError("MCDM not implemented")
 
-            plt.scatter(df_obj.iloc[:, 0], df_obj.iloc[:, 1], color='grey', marker='o')  # available
-            plt.scatter(df_obj.iloc[rank_predicted[:aux], 0], df_obj.iloc[rank_predicted[:aux], 1], color='red',
-                        marker='v')  # primeiro elemento
-            plt.scatter(df_obj.iloc[Q[:aux], 0], df_obj.iloc[Q[:aux], 1], color='black', marker='*')  # ultimo elemento
-            plt.ylim([-0.05, 2.2])
-            plt.xlim([-0.05, 2.2])
-            plt.show()
+            # plt.scatter(df_obj.iloc[:, 0], df_obj.iloc[:, 1], color='grey', marker='o')  # available
+            # plt.scatter(df_obj.iloc[rank_predicted[:aux], 0], df_obj.iloc[rank_predicted[:aux], 1], color='red',
+            #             marker='v')  # primeiro elemento
+            # plt.scatter(df_obj.iloc[Q[:aux], 0], df_obj.iloc[Q[:aux], 1], color='black', marker='*')  # ultimo elemento
+            # plt.ylim([-0.05, 2.2])
+            # plt.xlim([-0.05, 2.2])
+            # plt.show()
 
             # Computing tau similarity
             results[r]['tau'].append(norm_kendall(rank_aleatory, rank_predicted))
@@ -154,21 +173,51 @@ for r in recomm_engines:
 
             # Update the ranking
             rank_aleatory = rank_predicted
+            tam += n_rec
 
-
+# Euclidean recommendation
 euc = pd.DataFrame(results['personalized']['tau'])
 euc['Recommendation'] = tipo_recomendacao
 euc['iteracao'] = list(range(0, int(total_samples_per_rec / n_rec))) * n_executions
 
+euc_ = pd.DataFrame(results['personalized']['rho'])
+euc_['Recommendation'] = tipo_recomendacao
+euc_['iteracao'] = list(range(0, int(total_samples_per_rec / n_rec))) * n_executions
+
+euc_rmse = pd.DataFrame(results['personalized']['rmse'])
+euc_rmse['Recommendation'] = tipo_recomendacao
+euc_rmse['iteracao'] = list(range(0, int(total_samples_per_rec / n_rec))) * n_executions
+
+# Aleatory recommendation
 ale = pd.DataFrame(results['aleatory']['tau'])
 ale['Recommendation'] = 'Aleatory'
 ale['iteracao'] = list(range(0, int(total_samples_per_rec / n_rec))) * n_executions
 
-data = pd.concat([euc, ale])
+ale_ = pd.DataFrame(results['aleatory']['tau'])
+ale_['Recommendation'] = 'Aleatory'
+ale_['iteracao'] = list(range(0, int(total_samples_per_rec / n_rec))) * n_executions
 
-g = sns.boxplot(data=data, hue='Recommendation', x='iteracao', y=0)
-g.set_yscale("log")
-_ = g.set(xlabel="Iteration", ylabel="Similarity")
-plt.axhline(y=.05, ls=':', color='red')
-plt.title(mcdm_pairwise_method)
+ale_rmse = pd.DataFrame(results['aleatory']['rmse'])
+ale_rmse['Recommendation'] = 'Aleatory'
+ale_rmse['iteracao'] = list(range(0, int(total_samples_per_rec / n_rec))) * n_executions
+
+tau = pd.concat([euc, ale])
+rho = pd.concat([euc_, ale_])
+rmse = pd.concat([euc_rmse, ale_rmse])
+
+fig, axes = plt.subplots(3, 1, sharex=True)
+fig.suptitle(mcdm_pairwise_method)
+for i, k in enumerate([tau, rho, rmse]):
+    sns.boxplot(data=k, hue='Recommendation', x='iteracao', y=0, ax=axes[i])
+axes[0].legend(loc="upper right", prop={'size': 8})
+axes[1].get_legend().remove()
+axes[2].get_legend().remove()
+
+axes[0].set_ylabel('Tau')
+axes[0].set_xlabel('')
+axes[1].set_ylabel('Rho')
+axes[1].set_xlabel('')
+axes[2].set_ylabel('RMSE')
+axes[2].set_xlabel('Iteration')
+
 plt.show()
