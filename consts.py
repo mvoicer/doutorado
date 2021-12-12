405973fb6502b3613e7fe496a662c3435f145861
Marcos Alves <m.voicer@gmail>
2021-12-11 23:52:24 -0300

    Create a function that return a nested dict for the results

--- consts.py
+++ consts.py
@@ -1,35 +1,30 @@
-if not True:
-    n_executions = 3                # número de vezes que vai rodar o algoritmo para tirar a média
+if True:
+    n_executions = 4                # número de vezes que vai rodar o algoritmo para tirar a média
     total_samples_per_rec = 51      # total de amostras a serem avaliadas a cada n_rec
     n_rec: int = 5                  # numero de amostras que são apresentadas ao decisor por vez
     CV: int = 5                     # number of cross-validation
     test_size: float = 0.2          # 80% train and 20% test
     top_n = n_rec                   # top n solutions
-    max_sample = 100
-if True:
-    n_executions = 3                # número de vezes que vai rodar o algoritmo para tirar a média
-    total_samples_per_rec = 11      # total de amostras a serem avaliadas a cada n_rec
-    n_rec: int = 2                  # numero de amostras que são apresentadas ao decisor por vez
+    max_sample = 120
+if not True:
+    n_executions = 2                # número de vezes que vai rodar o algoritmo para tirar a média
+    total_samples_per_rec = 31      # total de amostras a serem avaliadas a cada n_rec
+    n_rec: int = 5                  # numero de amostras que são apresentadas ao decisor por vez
     CV: int = 2                     # number of cross-validation
     test_size: float = 0.2          # 80% train and 20% test
     top_n = n_rec                   # top n solutions
-    max_sample = 15
+    max_sample = 51
 
-results = {'aleatory': {'tau': [],
-                        'rho': [],
-                        'accuracy': [],
-                        'mse': [],
-                        'rmse': [],
-                        'r2': [],
-                        'mape': []
-                        },
-           'personalized': {'tau': [],
-                         'rho': [],
-                         'accuracy': [],
-                         'mse': [],
-                         'rmse': [],
-                         'r2': [],
-                         'mape': []
-                         }
-           }
+list_algs = ['gbr', 'lasso', 'elasticnet', 'rf', 'ridge']
 recomm_engines = ['aleatory', 'personalized']
+list_metrics = ['tau', 'rho', 'accuracy', 'mse', 'rmse', 'r2', 'mape']
+
+def initialize_results():
+    results = {}
+    for alg in list_algs:
+        results[alg] = {}
+        for eng in recomm_engines:
+            results[alg][eng] = {}
+            for metr in list_metrics:
+                results[alg][eng][metr] = []
+    return results
