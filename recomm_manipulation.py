bc8d1103bd5a249bdd1d2ae4acd5325f3a8023c4
Marcos Alves <m.voicer@gmail>
2022-01-13 19:50:04 -0300

    Functions that implement the recommendation process -- including the initial

---
+++ recomm_manipulation.py
@@ -0,0 +1,30 @@
+import random
+from clusterization import clusterization
+
+
+def make_recommendation(most_similar, Q, tam, qtd_to_add):
+    # Recommend the closest solutions
+    for q in most_similar:
+        if (len(Q) <= (tam - qtd_to_add)) & (q not in Q):
+            Q.append(q)
+    # Recommend aleatory solutions
+    for _ in most_similar:
+        randomized = most_similar.copy()
+        random.shuffle(randomized)
+        for p in randomized:
+            if (len(Q) <= tam) & (p not in Q):
+                Q.append(p)
+            else:
+                continue
+    return Q
+
+
+def initial_recommendation(type, indexes, df_obj, length):
+    if type == 'aleatory':
+        temp = indexes.copy()
+        random.shuffle(temp)
+        Q = temp[0:length]
+    elif type == 'cluster':
+        Q = clusterization(df=df_obj, type='kmeans', tam=length)
+    N_Q = [x for x in indexes if x not in Q]
+    return Q, N_Q
