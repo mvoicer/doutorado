d1aa7cd7993a908cd5eddf7f4331b72533321249
Marcos Alves <m.voicer@gmail>
2022-01-13 19:51:06 -0300

    Just renamed and deleted files.

--- consts.py
+++
@@ -1,8 +0,0 @@
-
-
-
-# list_algs = ['gbr', 'lasso', 'elasticnet', 'rf', 'ridge']
-list_algs = ['gbr', 'rf']
-recomm_engines = ['aleatory', 'personalized']
-list_metrics = ['tau', 'rho', 'accuracy', 'mse', 'rmse', 'r2', 'mape']
-
