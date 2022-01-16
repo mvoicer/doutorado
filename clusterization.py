8f3eda35efbd82237b56f0a9d9c9827192f76a81
Marcos Alves <m.voicer@gmail>
2022-01-16 16:22:11 -0300

    Cleaning .py file

--- clusterization.py
+++ clusterization.py
@@ -17,38 +17,3 @@ def clusterization(df, type, tam):
         return chosen
     else:
         raise ValueError("Cluster technique not implemented")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def generateVectors(dim, n_vetores):
-#     def ncr(n, r):
-#         r = min(r, n - r)
-#         numer = reduce(op.mul, range(n, n - r, -1), 1)
-#         denom = reduce(op.mul, range(1, r + 1), 1)
-#         return numer / denom
-#
-#     nRef = int(ncr((dim + n_vetores - 1), (dim - 1)))
-#
-#     vectors = np.zeros((dim, nRef))
-#     combinations = np.array([(i) for i in combinations_with_replacement(range(1, (dim + 1)), n_vetores)])
-#
-#     for i in range(dim):
-#         aux = np.array(list(map(lambda x: x == (i + 1), (combinations))))
-#         for j in range(nRef):
-#             vectors[i, j] = sum(aux[j,])
-#
-#     print(np.transpose((vectors / n_vetores)) * 2)
-#
-#
-# generateVectors(2, 3)
