ad61b4f2da32d00c6d48bfd671afa0b8570f0985
Marcos Alves <m.voicer@gmail>
2022-02-05 23:22:35 -0300

    Weights aggregation - for GroupMCDM

---
+++ aggregate_weights.py
@@ -0,0 +1,11 @@
+
+
+
+def aggregate_weights(weights):
+    """ Aggregate the weights of different decision-makers
+
+    Parameters:
+        weights :   list of lists
+            Each internal list represents the weights/preferences of each decision-maker
+    """
+    return [sum(i) for i in zip(*weights)] / len(weights)
