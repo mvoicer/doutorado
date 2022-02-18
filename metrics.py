726374497fca5d58717b84079807a3db48897fb3
Marcos Alves <m.voicer@gmail>
2022-02-17 22:45:28 -0300

    Set multioutput equal to uniform_average

--- metrics.py
+++ metrics.py
@@ -1,24 +1,24 @@
-import numpy as np
-from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
 
 
-# https://stackoverflow.com/questions/47648133/mape-calculation-in-python
+# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_absolute_percentage_error.html
 def mape(y_pred, y_true):
-    real, y_pred = np.array(y_true), np.array(y_pred)
-    return np.mean(np.abs((real - y_pred) / real))*100
-
-# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html
-def accuracy(y_pred, y_true):
-    return accuracy_score(y_true, y_pred)
+    return mean_absolute_percentage_error(y_true, y_pred,
+                                          multioutput='uniform_average')
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_squared_error.html
 def mse(y_pred, y_true):
-    return mean_squared_error(y_true, y_pred, squared=True)
+    return mean_squared_error(y_true, y_pred,
+                              multioutput='uniform_average',
+                              squared=True)
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_squared_error.html
 def rmse(y_pred, y_true):
-    return mean_squared_error(y_true, y_pred, squared=False)
+    return mean_squared_error(y_true, y_pred,
+                              multioutput='uniform_average',
+                              squared=False)
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.r2_score.html
 def r2(y_pred, y_true):
-    return r2_score(y_true, y_pred)
+    return r2_score(y_true, y_pred,
+                    multioutput='uniform_average')
