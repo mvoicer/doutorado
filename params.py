f8b347fa2cdf91499c63369035dc53ca9e152047
Marcos Alves <m.voicer@gmail>
2022-02-17 22:48:00 -0300

    Parameters updated

--- params.py
+++ params.py
@@ -1,11 +1,18 @@
 
 
-list_metrics = ['tau', 'rho', 'accuracy', 'mse', 'rmse', 'r2', 'mape']
+list_metrics = ['tau', 'rho', 'mse', 'rmse', 'r2', 'mape']
 
 # Apenas checa se está fazendo testes no algoritmo principal ou não (neste caso, usa o dataframe completo)
+# def load_params():
+#     n_executions = 3                # número de vezes que vai rodar o algoritmo para tirar a média
+#     total_samples_per_rec = 51      # total de amostras a serem avaliadas a cada n_rec
+#     max_sample = 120
+#     CV=5                            # number of cross-validation
+#     return n_executions, total_samples_per_rec, max_sample, CV
+
 def load_params():
     n_executions = 3                # número de vezes que vai rodar o algoritmo para tirar a média
-    total_samples_per_rec = 51      # total de amostras a serem avaliadas a cada n_rec
-    max_sample = 120
+    total_samples_per_rec = 31      # total de amostras a serem avaliadas a cada n_rec
+    max_sample = 50
     CV=5                            # number of cross-validation
     return n_executions, total_samples_per_rec, max_sample, CV
