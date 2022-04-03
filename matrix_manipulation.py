ec8bae84ccf3d98be4ae50a0b04f03f8ca8a4e78
Marcos Alves <m.voicer@gmail.com>
2022-04-03 13:07:06 -0300

    Optimizing function - new parameter to use or not all the combinations (A1+A2, A1+A1..)

--- matrix_manipulation.py
+++ matrix_manipulation.py
@@ -2,44 +2,35 @@ import pandas as pd
 import numpy as np
 
 
-def create_subsample(df_var, df_pref, nobj, index):
+def create_subsample(df_var, df_pref, nobj, index, all_comb):
     """
     Create sub-dataframes with the features (alternatives) and target (value in the objective space).
     """
-
-    # Create a df_aux that receive the features concatenated (objectives) and targets (preference)
     sub_df = pd.DataFrame(np.zeros((len(index), df_var.shape[1] * 2 + nobj)))
     cont = 0
-    for i in index:
-        for j in index:
-            # Concatenate the two rows - i.e. values of the objectives
-            # and the preference between the two objectives
-            sub_df.loc[cont] = pd.concat([df_var.loc[i], df_var.loc[j], df_pref.loc[i, j]], axis=0, ignore_index=True)
-            cont += 1
+    if all_comb is True:
+        for i in index:
+            for j in index:
+                # Concatenate the two rows - i.e. values of the objectives
+                # and the preference between the two objectives
+                sub_df.loc[cont] = pd.concat([df_var.loc[i], df_var.loc[j], df_pref.loc[i, j]], axis=0,
+                                             ignore_index=True)
+                cont += 1
+    else:
+        for i in index:
+            for j in index:
+                if i != j:
+                    sub_df.loc[cont] = pd.concat([df_var.loc[i], df_var.loc[j], df_pref.loc[i, j]], axis=0,
+                                                 ignore_index=True)
+                else:
+                    continue
+                cont += 1
     X = sub_df.iloc[:, :-nobj]
     y = sub_df.iloc[:, -nobj:]
     return X, y
 
-def _create_subsample(df_var, df_pref, nobj, index):
-    """
-    Create sub-dataframes with the features (alternatives) and target (value in the objective space).
-    """
 
-    # Create a df_aux that receive the features concatenated (objectives) and targets (preference)
-    sub_df = pd.DataFrame(np.zeros((len(index), df_var.shape[1] * 2 + nobj)))
-    cont = 0
-    for i in index:
-        for j in index:
-            if i != j:
-                sub_df.loc[cont] = pd.concat([df_var.loc[i], df_var.loc[j], df_pref.loc[i, j]], axis=0, ignore_index=True)
-            else:
-                continue
-            cont += 1
-    X = sub_df.iloc[:, :-nobj]
-    y = sub_df.iloc[:, -nobj:]
-    return X, y
-
-def merge_matrices(idx_N_Q, preference_matrix, ml_predicted):
+def merge_matrices(idx_N_Q, preference_matrix, ml_predicted, all_comb):
     """
     Replace the predicted values in the preference matrix to calculate
     if the rankings (predicted vs preference) are equal or not.
@@ -49,45 +40,25 @@ def merge_matrices(idx_N_Q, preference_matrix, ml_predicted):
     :return: dataframe merged with the real values and the predicted values
     """
     df_merged = preference_matrix.copy()
-    nobj = ml_predicted.shape[1]
 
-    # Gera todas as combinações do N-Q
     comb_idx = []
-    for i in idx_N_Q:
-        for k in idx_N_Q:
-            comb_idx.append(tuple([i, k]))
+    if all_comb is True:
+        # Gera todas as combinações do N-Q
+        for i in idx_N_Q:
+            for k in idx_N_Q:
+                comb_idx.append(tuple([i, k]))
+    else:
+        # Generate the combinations with the N_Q indexes
+        comb_idx = []
+        for i in idx_N_Q:
+            for k in idx_N_Q:
+                if i != k:
+                    comb_idx.append(tuple([i, k]))
 
     results = pd.DataFrame()
     x = 0
     for k in range(0, df_merged.shape[1], df_merged.shape[0]):
         m = df_merged.iloc[:, k:k+df_merged.shape[0]].to_numpy()
-
-        for i, idx in enumerate(comb_idx):
-            m[idx] = ml_predicted.values[i, x]
-        x += 1
-        m = pd.DataFrame(m)
-        results = pd.concat([results, m], ignore_index=False, axis=1)
-    return results
-
-def _merge_matrices(idx_N_Q, preference_matrix, ml_predicted):
-    """
-    Replace the values predicted by the ML in the preference matrix to measure the errors - i.e., if the ML is
-    predicting well.
-    """
-    df_merged = preference_matrix.copy()
-
-    # Generate the combinations with the N_Q indexes
-    comb_idx = []
-    for i in idx_N_Q:
-        for k in idx_N_Q:
-            if i != k:
-                comb_idx.append(tuple([i, k]))
-
-    # In each N_Q combination index, insert the ML predicted value.
-    results = pd.DataFrame()
-    x = 0
-    for y in range(0, df_merged.shape[1], df_merged.shape[0]):
-        m = df_merged.iloc[:, y:y+df_merged.shape[0]].to_numpy()
 
         for i, idx in enumerate(comb_idx):
             m[idx] = ml_predicted.values[i, x]
--- run_recommender.py
+++ run_recommender.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 import joblib
 import random
-from matplotlib import pyplot as plt
 import math
 
-from matrix_manipulation import create_subsample, merge_matrices, _create_subsample, _merge_matrices
+from matrix_manipulation import create_subsample, merge_matrices
 from ml_models import fine_tunning
 from correlation import norm_kendall, spearman_rho
 from params import *
@@ -22,27 +21,25 @@ np.seterr(divide='ignore', invalid='ignore')  # ignora erros de divisão por 0
 def run_recommender(dataframe, n_rec, mcdm_method, weights, cost_benefit, percent_random,
                     initial_recomm, similarity_measure, ml_method, date, plot_pareto_front,
                     plot_recommended_solutions):
-    # How to save the results
-    # global y_train, X_train
+    # How to save the results -- define name
     path_to_save = (mcdm_method + '_' + str(
         percent_random) + 'initrandom_sim' + similarity_measure + '_' + ml_method + '_' + date).lower()
 
     # Load variables
     n_executions, total_samples_per_rec, max_sample, CV = load_params()
     df_var, df_obj = load_dataset(df=dataframe, n_samples=max_sample)
-    # print('')
-    # print(df_var.to_string())
+
     npop, nvar = df_var.shape
     nobj = df_obj.shape[1]
     results = initialize_results()
+    # results_treino = initialize_results()
 
     # Calculate the distances/similarities among the solutions
     df_dist = calculate_similarities(df_obj, similarity_measure)
 
     # Generate the preferences
     pc_matrix, rank_mcdm = generate_preferences(mcdm_method, df_obj, weights=weights, cb=cost_benefit)
-    # print('')
-    # print(pc_matrix.to_string())
+
     if plot_pareto_front is True:
         Visualization.plot_pareto_front(df_obj, rank_mcdm, n_rec)
 
@@ -63,8 +60,8 @@ def run_recommender(dataframe, n_rec, mcdm_method, weights, cost_benefit, percen
             if len(Q) == 0:
                 Q, N_Q = initial_recommendation(type_rec=initial_recomm, indexes=indexes, df_obj=df_obj, length=n_rec)
                 # Train and test set
-                X_train, y_train = _create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=Q)
-                X_test, y_test = _create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=N_Q)
+                X_train, y_train = create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=Q, all_comb=False)
+                X_test, y_test = create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=N_Q, all_comb=False)
             else:
                 # Calculate the distances/similarities among the solutions
                 most_similar = df_dist.iloc[rank_aleatory[0]].sort_values(ascending=True).index.to_list()
@@ -75,7 +72,7 @@ def run_recommender(dataframe, n_rec, mcdm_method, weights, cost_benefit, percen
                 # Get the preferences for the new alternatives (entrance) and
                 # for the last one in Q (for the condorcet/regularization) and
                 # merge to the train and test set
-                X_train_, y_train_ = _create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=entrance + Q)
+                X_train_, y_train_ = create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=entrance + Q, all_comb=False)
                 X_train_cond, y_train_cond = condorcet(Q, entrance, pc_matrix, df_var, nobj)
                 X_train = pd.concat([X_train_, X_train_cond, X_train], axis=0, ignore_index=True)
                 y_train = pd.concat([y_train_, y_train_cond, y_train], axis=0, ignore_index=True)
@@ -85,19 +82,17 @@ def run_recommender(dataframe, n_rec, mcdm_method, weights, cost_benefit, percen
 
                 # Generate the remaining ones
                 N_Q = [x for x in indexes if x not in Q]
-                X_test, y_test = _create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=N_Q)
-            # print('Q', Q)
+                X_test, y_test = create_subsample(df_var=df_var, df_pref=pc_matrix, nobj=nobj, index=N_Q, all_comb=False)
+
             if plot_recommended_solutions is True:
                 Visualization.plot_recommended_solutions(df_obj, Q, rank_aleatory, n_rec)
 
             # Fine tunning in the 1st execution, otherwise load the model saved
             if temp_error > accepted_error:
-                # print('Treinou e o erro atual é: ', temp_error)
                 tuned_model = fine_tunning(CV, X_train, y_train, algorithm=ml_method)
                 tuned_model.fit(X_train, y_train)
                 joblib.dump(tuned_model, "tunned_models/" + path_to_save + '.gz')
             else:
-                # print('Testou. Erro: ', temp_error)
                 tuned_model = joblib.load("tunned_models/" + path_to_save + ".gz")
 
             # Model evaluation
@@ -118,7 +113,7 @@ def run_recommender(dataframe, n_rec, mcdm_method, weights, cost_benefit, percen
             results['mape'].append(mape(y_pred=y_pred, y_true=y_test))
 
             # Merge the predictions of the df train and df test
-            df_merged = _merge_matrices(N_Q, pc_matrix, y_pred)
+            df_merged = merge_matrices(N_Q, pc_matrix, y_pred)
 
             # Calculate the predicted ranking
             rank_predicted = calculate_new_ranking(mcdm_method, df_merged, weights=weights, npop=npop, nobj=nobj)
