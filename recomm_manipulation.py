34a341d91e5a5b7a56fa3117642a70328571c167
Marcos Alves <m.voicer@gmail.com>
2022-10-14 13:08:48 +0100

    Comments in code

--- recomm_manipulation.py
+++ recomm_manipulation.py
@@ -3,8 +3,17 @@ from clusterization import clusterization
 
 
 def make_recommendation(most_similar, Q, n_rec, qtd_aleatory_to_add):
-
-    # Recommend the closest solutions
+    """
+    Summary:
+        Recommend the closest solutions
+    Parameters:
+        most_similar:           closest/similar solutions ranked highest in the previous iteration
+        Q:                      list of the current solutions
+        n_rec:                  number of solutions to be recommended per iteration
+        qtd_aleatory_to_add:    quantity of solutions to be recommended in this iteration
+    Returns:
+        List of solutions to be recommended in the current iteration
+    """
     new_recommendation = []
     [new_recommendation.append(x) for x in most_similar if (len(new_recommendation) < (n_rec - qtd_aleatory_to_add)) & (x not in Q)]
 
@@ -18,8 +27,16 @@ def make_recommendation(most_similar, Q, n_rec, qtd_aleatory_to_add):
 
 def initial_recommendation(type_rec, indexes, df_obj, length):
     """
-    rand: aleatory recommendations
-    cluster: use kmeans technique and select 1 solution per cluster
+    Summary:
+        Define the solutions to be chosen in the 1st iteration
+    Parameters:
+        type_rec:
+            rand:       aleatory recommendations
+            cluster:    use kmeans technique and select 1 solution per cluster
+        indexes:    indexes of the variables in decision space
+        df_obj:     solutions in solutions space
+        length:     n_rec- number of solutions to be recommended per iteration
+        n_rec:                  number of solutions to be recommended per iteration
     """
     if type_rec == 'rand':
         temp = indexes.copy()
