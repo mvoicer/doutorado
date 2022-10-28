80019cdecf8949d97380095cfabb2a6c0e04bec5
Marcos Alves <m.voicer@gmail.com>
2022-10-28 11:59:08 -0300

    Code optimization. Remove some IF and then replace with fill_diagonal.

--- distance.py
+++ distance.py
@@ -13,14 +13,10 @@ class Distance:
         Input: Pareto front solutions
         Output: Squared matrix with the distance among the solutions.
         """
-        # TODO: optimize code - não é necessário calcular a diagnonal e nem os dois lados da matriz
         df_aux = pd.DataFrame(np.zeros((self.df.shape[0], self.df.shape[0])))
         for r in range(self.df.shape[0]):
             for c in range(self.df.shape[0]):
-                if r != c:
-                    df_aux.loc[r, c] = distance.euclidean(self.df.iloc[r], self.df.iloc[c])
-                else:
-                    df_aux.loc[r, c] = 0
+                df_aux.loc[r, c] = distance.euclidean(self.df.iloc[r], self.df.iloc[c])
         return df_aux
 
     def cosine(self):
@@ -28,13 +24,14 @@ class Distance:
         Compute the cosine similarity
         :return: cosine similarity in [-1 1] interval.
         """
+        #https://stackoverflow.com/questions/58381092/difference-between-cosine-similarity-and-cosine-distance
+        #https://stackoverflow.com/questions/18424228/cosine-similarity-between-2-number-lists
         df_aux = pd.DataFrame(np.zeros((self.df.shape[0], self.df.shape[0])))
         for r in range(self.df.shape[0]):
             for c in range(self.df.shape[0]):
-                if r != c:
-                    df_aux.loc[r, c] = 1 - distance.cosine(self.df.iloc[r], self.df.iloc[c])
-                else:
-                    df_aux.loc[r, c] = 0
+                # Subtract 1 (see link above) to calculate similarity, rather than distance.
+                df_aux.loc[r, c] = 1 - distance.cosine(self.df.iloc[r], self.df.iloc[c])
+
+        np.fill_diagonal(df_aux.to_numpy(), 0)  # preenche diagonal principal com 0
+
         return df_aux
-
-
