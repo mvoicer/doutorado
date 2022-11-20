97b2c4c9013351fc41a857d35e4ce688c595e5ca
Marcos Alves <m.voicer@gmail.com>
2022-11-19 23:38:20 -0300

    Removed 'max_sample'

--- params.py
+++ params.py
@@ -6,13 +6,11 @@ list_metrics = ['tau', 'rho', 'mse', 'rmse', 'r2', 'mape', 'mcdm']
 # def load_params():
 #     n_executions = 5
 #     total_samples_per_rec = 150
-#     max_sample = 200
 #     CV=3                            # number of cross-validation
 #     return n_executions, total_samples_per_rec, max_sample, CV
 
 def load_params():
-    n_executions = 4                # número de vezes que vai rodar o algoritmo para tirar a média
-    total_samples_per_rec = 200      # total de amostras a serem avaliadas a cada n_rec
-    max_sample = 200
-    CV=3                            # number of cross-validation
-    return n_executions, total_samples_per_rec, max_sample, CV
+    n_executions = 5                # número de vezes que vai rodar o algoritmo para tirar a média
+    total_samples_per_rec = 76      # total de amostras a serem avaliadas a cada n_rec
+    CV=4                            # number of cross-validation
+    return n_executions, total_samples_per_rec, CV
