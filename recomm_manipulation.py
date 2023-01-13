e326d7b6441dfdede678c509c3af13a3ee2543f9
Marcos Alves <m.voicer@gmail.com>
2023-01-13 16:17:31 -0300

    Code improvements.

--- recomm_manipulation.py
+++ recomm_manipulation.py
@@ -12,7 +12,7 @@ def make_recommendation(most_similar, Q, n_rec, qtd_aleatory_to_add):
         n_rec:                  number of solutions to be recommended per iteration
         qtd_aleatory_to_add:    quantity of solutions to be recommended in this iteration
     Returns:
-        List of solutions to be recommended in the current iteration
+        new_recommendation: List of solutions to be recommended in the current iteration
     """
     new_recommendation = []
     [new_recommendation.append(x) for x in most_similar if (len(new_recommendation) < (n_rec - qtd_aleatory_to_add)) & (x not in Q)]
@@ -31,25 +31,22 @@ def initial_recommendation(type_rec, indexes, df_obj, length):
         Define the solutions to be chosen in the 1st iteration
     Parameters:
         type_rec: Type of recommendation. It can be rand or using clustering (kmeans or agglomerative)
-            rand:       aleatory recommendations
-            kmeans:    use kmeans technique and select 1 solution per cluster
-            agglomerative:    use agglomerative clustering technique and select 1 solution per cluster
-        indexes:    indexes of the variables in decision space
-        df_obj:     solutions in solutions space
-        length:     (equal n_rec) number of solutions to be recommended per iteration
-        n_rec:                  number of solutions to be recommended per iteration
+            rand: aleatory recommendations
+            kmeans: use kmeans technique and select 1 solution per cluster
+            agglomerative: use agglomerative clustering technique and select 1 solution per cluster
+        indexes: indexes of the variables in decision space
+        df_obj: solutions in solutions space
+        length: (equal n_rec) number of solutions to be recommended per iteration
+        n_rec:  number of solutions to be recommended per iteration
     Returns:
     Q (list): List of indexes of the recommended solutions.
     N_Q (list): List of indexes of the non-recommended solutions.
     """
-    global Q
-    if type_rec == 'rand':
-        temp = indexes.copy()
-        random.shuffle(temp)
-        Q = temp[0:length]
-    elif type_rec == 'kmeans':
-        Q = clusterization(df=df_obj, cluster_technique='kmeans', tam=length)
-    elif type_rec == 'agglomerative':
-        Q = clusterization(df=df_obj, cluster_technique='agglomerative', tam=length)
+    if type_rec not in ['rand', 'kmeans', 'agglomerative']:
+        raise ValueError(f'Invalid recommendation type: {type_rec}')
+    elif type_rec == 'rand':
+        Q = random.sample(indexes, length)
+    else:
+        Q = clusterization(df=df_obj, cluster_technique=type_rec, tam=length)
     N_Q = [x for x in indexes if x not in Q]
     return Q, N_Q
