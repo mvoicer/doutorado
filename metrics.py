f24bddfd023ec9d7f45765edeebdb410f4abc0ef
Marcos Alves <m.voicer@gmail.com>
2023-01-13 19:44:25 -0300

    KDT implementation.

--- metrics.py
+++ metrics.py
@@ -12,6 +12,16 @@ def norm_kendall(r1, r2):
     - r1: list, first list to compare.
     - r2: list, second list to compare.
 
+    Returns:
+    - Normalized Kendall tau distance between r1 and r2.
+    """
+    n = len(r1)
+    assert len(r2) == n, "Both lists have to be of equal length"
+    i, j = np.meshgrid(np.arange(n), np.arange(n))
+    a = np.argsort(r1)
+    b = np.argsort(r2)
+    ndisordered = np.logical_or(np.logical_and(a[i] < a[j], b[i] > b[j]), np.logical_and(a[i] > a[j], b[i] < b[j])).sum()
+    return (ndisordered / (n * (n - 1))).round(4)
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_absolute_percentage_error.html
 def mape(y_pred, y_true):
