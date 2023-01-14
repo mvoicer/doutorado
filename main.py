b2b6f671325744486d380001cc2620bb7067c29c
Marcos Alves <m.voicer@gmail.com>
2023-01-14 17:39:49 -0300

    Teste.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from run_recommender import run_recommender
 
+
 results = run_recommender(dataframe='NSGAIII_GPD04_M3',
                           n_rec=5,                                      # int: qtd solutions to be evaluated per time
                           mcdm_method='AHP',                             # AHP, Promethee
