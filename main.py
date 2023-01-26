abfb74f0bb81c26cee3e2bea4e92d55bd16f4874
Marcos Alves <m.voicer@gmail.com>
2023-01-25 21:14:31 -0300

    Fixing some parameters.

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from run_recommender import run_recommender
 
 results = run_recommender(dataframe='NSGAIII_GPD04_M3',
                           n_rec=5,                                      # int: qtd solutions to be evaluated per time
-                          mcdm_method='AHP',                             # AHP, Promethee
+                          mcdm_method='AHP',                            # AHP
                           weights=None,
                           cost_benefit=None,
-                          percent_random=0.5,                           # {0-1}% float. Aleatory solutions to be recomm
-                          initial_recomm='cluster',                     # rand, cluster
-                          similarity_measure='euc',                     # cos, euc
-                          ml_method='rf',                               #'gbr', 'lasso', 'elast', 'rf', 'ridge'
+                          percent_random=0.5,                           # {0-1}% float.
+                          initial_recomm='cluster',                     # rand, kmeans, agglomerative
+                          similarity_measure='euclidean',               # cosine, euclidean, manhattan
+                          ml_method='rf',                               # gbr, rf
                           date='10102022',
                           n_executions = n_executions,
                           total_samples_per_rec = total_samples_per_red,
