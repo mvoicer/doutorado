52d0def238ab71264cccef282bc87ca818c86767
Marcos Alves <m.voicer@gmail.com>
2023-03-02 23:49:22 -0300

    Reupload code.

---
+++ distance.py
@@ -0,0 +1,26 @@
+from sklearn.metrics import pairwise_distances
+
+
+def calculate_similarities(df_obj, simm_approach):
+    """
+    Summary:
+        Calculate the distance among all the solutions based on 'simm_approach'.
+    Parameters:
+        df_obj: dataframe with the values in objective space
+        simm_approach: similarity measure to be used.
+            - 'cos': cosine similarity
+            - 'euc': euclidean distance
+            - 'man': manhattan distance
+    Returns:
+        dataframe with the distances
+    """
+    if simm_approach == 'cos':
+        #TODO: Ver distancia e similaridade.
+        df_dist = 1 - pairwise_distances(df_obj, metric='cosine')
+    elif simm_approach == 'euc':
+        df_dist = pairwise_distances(df_obj, metric='euclidean')
+    elif simm_approach == 'manhattan':
+        df_dist = pairwise_distances(df_obj, metric='manhattan')
+    else:
+        raise ValueError('Distance indicated is not implemented')
+    return df_dist
