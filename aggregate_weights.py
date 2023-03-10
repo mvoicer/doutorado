56fc5fa590cebb69f138af7e8b1961b0e3982e7c
Marcos Alves <m.voicer@gmail.com>
2023-03-09 22:15:20 -0300

    Included function to aggregate the weights based on geometric mean.

--- aggregate_weights.py
+++ aggregate_weights.py
@@ -1,4 +1,5 @@
-
+from functools import reduce
+import math
 
 
 def aggregate_weights(weights):
@@ -8,3 +9,9 @@ def aggregate_weights(weights):
             Each internal list represents the weights/preferences of each decision-maker
     """
     return [sum(i) for i in zip(*weights)] / len(weights)
+
+
+def aggregate_weights_geometric_mean(weights):
+    # Aggregate the weights of different decision-makers using geometric mean
+    produto = reduce(lambda x, y: [x[i]*y[i] for i in range(len(x))], weights)
+    return math.pow(reduce(lambda x, y: x*y, produto), 1/len(weights))
