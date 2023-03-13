b82baaa2d2c2e7ae76c8621b32dcc7c381008be5
Marcos Alves <m.voicer@gmail.com>
2023-03-13 17:02:41 -0300

    Recreated params.py

---
+++ params.py
@@ -0,0 +1,11 @@
+# List of parameters, variables and hiperparameters used
+
+# List of metrics to compute
+metrics = ['mcdm', 'tau', 'pos', 'dbs', 'mse', 'rmse', 'r2', 'mape', 'nq']
+
+
+# ML
+CV = 5  # Number of cross-validations
+epsilon = 0.05
+temp_error = 1.0
+number_queries = 0
