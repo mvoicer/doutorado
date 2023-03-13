f8530cf182e9725be89d4ef95377db1ff44a83e1
Marcos Alves <m.voicer@gmail.com>
2023-03-13 16:58:45 -0300

    Changed from RandomizedSearchCV to Optuna hyperparameter optimizer.

--- ml_models.py
+++ ml_models.py
@@ -1,82 +1,55 @@
 from sklearn.multioutput import MultiOutputRegressor
-from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn import linear_model
 import optuna
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from params import CV
 
 
-def fine_tunning(CV, X, y, algorithm):
-    # if algorithm == 'gbr':
-    #     model = MultiOutputRegressor(GradientBoostingRegressor())
-    #     hyperparameters = dict(estimator__learning_rate=[0.05],
-    #                            estimator__n_estimators=[50, 500, 1000],
-    #                            estimator__min_samples_split=[7, 10],
-    #                            estimator__max_depth=[15, 30],
-    #                            estimator__min_samples_leaf=[1, 2, 10])
-    #     grid_search = GridSearchCV(model,
-    #                                hyperparameters,
-    #                                cv=CV,
-    #                                n_jobs=-1,
-    #                                refit=True,
-    #                                verbose=True,
-    #                                return_train_score=True)
-    #     hyperparameters_tuning = grid_search.fit(X, y)
-    #     tuned_model = hyperparameters_tuning.best_estimator_
-    #     return tuned_model
-
+def fine_tunning(X, y, algorithm):
     if algorithm == 'gbr':
-        # Cria o objeto de estudo
         study = optuna.create_study()
 
-        # Define a função de objetivo
         def objective(trial):
             model = MultiOutputRegressor(GradientBoostingRegressor(
                 learning_rate=trial.suggest_loguniform('learning_rate', 1e-4, 1e-1),
-                n_estimators=trial.suggest_int('n_estimators', 50, 1000),
-                min_samples_split=trial.suggest_int('min_samples_split', 2, 15),
-                max_depth=trial.suggest_int('max_depth', 5, 30),
-                min_samples_leaf=trial.suggest_int('min_samples_leaf', 1, 10)
+                n_estimators=trial.suggest_int('n_estimators', 1, 1001, step=100),
+                min_samples_split=trial.suggest_int('min_samples_split', 2, 7, step=1),
+                max_depth=trial.suggest_int('max_depth', 2, 30, step=5),
+                min_samples_leaf=trial.suggest_int('min_samples_leaf', 1, 5, step=1)
             ))
 
-            # Executa a validação cruzada
             scores = cross_val_score(model, X, y, cv=CV, n_jobs=-1)
-
-            # Retorna a média das métricas de desempenho
             return np.mean(scores)
 
         # Executa a busca de hiperparâmetros
         study.optimize(objective, n_trials=100)
 
         # Recupera o melhor modelo
-        best_model = study.best_trial.user_attrs['model']
-        return best_model
-
-    # elif algorithm == 'rf':
-    #     model = MultiOutputRegressor(RandomForestRegressor())
-    #     hyperparameters = dict(estimator__n_estimators=[10, 50, 100, 1000],
-    #                            estimator__max_features=["log2", "sqrt"],
-    #                            estimator__max_depth=[5, 15, 30, 80],
-    #                            estimator__min_samples_leaf=[3, 4, 5],
-    #                            estimator__min_samples_split=[2, 4, 10, 20])
-    #     grid_search = GridSearchCV(model,
-    #                                hyperparameters,
-    #                                cv=CV,
-    #                                n_jobs=-1,
-    #                                refit=True,
-    #                                verbose=True,
-    #                                return_train_score=True)
-    #     hyperparameters_tuning = grid_search.fit(X, y)
-    #     tuned_model = hyperparameters_tuning.best_estimator_
-    #     return tuned_model
+        best_model = study.best_trial.user_attrs.get('model')
+        if best_model is not None:
+            return best_model
+        else:
+            best_params = study.best_params
+            best_model = MultiOutputRegressor(GradientBoostingRegressor(
+                learning_rate=best_params['learning_rate'],
+                n_estimators=best_params['n_estimators'],
+                min_samples_split=best_params['min_samples_split'],
+                max_depth=best_params['max_depth'],
+                min_samples_leaf=best_params['min_samples_leaf']
+            ))
+            best_model.fit(X, y)
+            study.best_trial.user_attrs['model'] = best_model
+            return best_model
 
     elif algorithm == 'rf':
         def objective(trial):
-            n_estimators = trial.suggest_int('estimator__n_estimators', 10, 1000)
-            max_features = trial.suggest_categorical('estimator__max_features', ["log2", "sqrt"])
-            max_depth = trial.suggest_int('estimator__max_depth', 5, 80)
-            min_samples_leaf = trial.suggest_int('estimator__min_samples_leaf', 3, 5)
-            min_samples_split = trial.suggest_int('estimator__min_samples_split', 2, 20)
-            criterion = trial.suggest_categorical('estimator__criterion',
+            n_estimators = trial.suggest_int('n_estimators', 1, 1001, step=100)
+            max_features = trial.suggest_categorical('max_features', ["log2", "sqrt"])
+            max_depth = trial.suggest_int('max_depth', 3, 30, step=5)
+            min_samples_leaf = trial.suggest_int('min_samples_leaf', 3, 5, step=1)
+            min_samples_split = trial.suggest_int('min_samples_split', 2, 5, step=1)
+            criterion = trial.suggest_categorical('criterion',
                                                   ['squared_error', 'absolute_error', 'poisson'])
 
             model = MultiOutputRegressor(RandomForestRegressor(n_estimators=n_estimators,
